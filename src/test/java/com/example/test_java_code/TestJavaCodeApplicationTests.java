6acdadc1c8fa479b20f6de9694e5ff789e675a4f
linda <charming.linda85@gmail.com>
2024-12-16 12:43:18 +0300

    Initial commit

---
+++ src/test/java/com/example/test_java_code/TestJavaCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_java_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJavaCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
